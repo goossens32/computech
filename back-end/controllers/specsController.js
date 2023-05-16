491ad961f6b809f53ad336c08e167c1b1943270d
goossens32 <cgp00@iesemilidarder.com>
2023-05-16 13:41:59 +0200

    Started specs controller for page

--- back-end/controllers/specsController.js
+++ back-end/controllers/specsController.js
@@ -1,2 +1,11 @@
 import Specs from '../models/Specs.js';
 
+export const showAllSpecs = async (req, res) => {
+    try {
+        const documents = await Specs.find({});
+        res.json(documents);
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
