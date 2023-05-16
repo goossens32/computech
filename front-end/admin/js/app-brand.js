f746669a72bb758504d837d5cef61d5bf8f45afd
goossens32 <cgp00@iesemilidarder.com>
2023-05-16 13:37:38 +0200

    Completed application for admin brands

---
+++ front-end/admin/js/app-brand.js
@@ -0,0 +1,138 @@
+import BrandService from "./services/brandService.js";
+import Modal from "../js/components/Modal.js";
+
+const bodyContainer = document.querySelector('#tbody-container');
+const formItems =  document.querySelector('.brand-form');
+const btnInsert = document.querySelector('#btn-insert');
+const btnUpdate = document.querySelector('#btn-update');
+const btnCancel = document.querySelector('.btn-cancel');
+
+
+// Form constants
+const nameInput = document.querySelector('#name-input');
+const descInput = document.querySelector('#desc-input');
+let currentBrand = null;
+
+
+const newBrand = () => {
+    const name = nameInput.value;
+    const description = descInput.value;
+    const newBrand = {name, description};
+
+    Modal.confirmInsert();
+    // --------------------------------------------------
+    BrandService.insert(newBrand).then(data => {
+        console.log(data);
+        renderBrands();
+        formItems.reset();
+    });
+}
+
+const editBrand = (id) => {
+    BrandService.getItemById(id).then(data => {
+        currentBrand = data;
+        nameInput.value = data.name;
+        descInput.value = data.description;
+    });
+    btnInsert.classList.replace("d-inline", "d-none");
+    btnUpdate.classList.replace("d-none", "d-inline");
+}
+
+const updateBrand = () => {
+    const id = currentBrand.id;
+    const name = nameInput.value;
+    const description = descInput.value;
+    const brand = {id, name, description};
+    console.log(brand);
+
+    // Pasas parámetros de brand a Update
+    BrandService.update(brand).then(data => {
+        currentBrand = null;
+        formItems.reset();
+        renderBrands();
+    });
+    btnInsert.classList.replace("d-none", "d-inline");
+    btnUpdate.classList.replace("d-inline", "d-none");
+}
+
+const deleteBrand = (id) => {
+    Modal.waringDelete();
+    // --------------------------------------------------
+    BrandService.delete(id).then(data => {
+        console.log(data);
+        renderBrands();
+    })
+}
+
+const validateForm = (event) => {
+    event.preventDefault();
+    if (event.target.id === "btn-insert") {
+        newBrand();
+    } else if (event.target.id === "btn-update") {
+        updateBrand();
+    } else {
+        console.log("ID Button not found");
+    }
+}
+
+const populateTableBrands = (items) => {
+    items.forEach((e, i) => {
+        bodyContainer.innerHTML += `
+            <tr>
+                <td style="text-align: center;">${i + 1}</td>
+                <td>${e.name}</td>
+                <td>${e.description}</td>
+                <td style="display:flex;">
+                    <div><button id="btn-delete-${e.id}" class="btn-delete"><i class="fa-regular fa-trash-can fa-lg"></i></button></div>
+                    <div><button id="btn-edit-${e.id}" class="btn-edit"><i class="fa-solid fa-pencil fa-lg"></i></button></div>
+                </td>
+            </tr>
+        `
+    });
+
+    const buttonsDelete = document.querySelectorAll('.btn-delete');
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", function () {
+            let id = this.id.split("-")[2];
+            console.log(id);
+            deleteBrand(id);
+        })
+    });
+
+    const buttonsEdit = document.querySelectorAll('.btn-edit');
+    buttonsEdit.forEach(button => {
+        button.addEventListener("click", function () {
+            let id = this.id.split("-")[2];
+            editBrand(id);
+        })
+    });
+}
+
+const renderBrands = (search) => {
+    bodyContainer.innerHTML = "";
+    if (search) {
+        BrandService.searchItemByName(search).then(items => {
+            if (items.length === 0) {
+                bodyContainer.innerHTML = "No se ha encontrado ninguna marca";
+            } else {
+                populateTableBrands(items)
+            }
+        })
+    } else {
+        BrandService.getItemsList().then(items => {
+            populateTableBrands(items);
+        })
+    }
+}
+
+const init = () => {
+    renderBrands();
+
+    // Reinicio de formulario cuando se pulsa en cancelar
+    btnCancel.addEventListener('click', function() {
+        formItems.reset();
+    })
+    btnInsert.addEventListener('click', validateForm);
+    btnUpdate.addEventListener('click', validateForm);
+}
+init();
